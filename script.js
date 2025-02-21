10e8d90ec2d86255163be17ed9095aca64c1c48c
Babak Rahimi <babak82@gmail.com>
2025-02-21 22:50:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const countdown = () => {
+    const targetDate = new Date("September 30, 2025 23:59:59").getTime();
+    setInterval(() => {
+        const now = new Date().getTime();
+        const timeLeft = targetDate - now;
+
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        document.getElementById("days").innerText = days;
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("minutes").innerText = minutes;
+        document.getElementById("seconds").innerText = seconds;
+    }, 1000);
+};
+
+countdown();
